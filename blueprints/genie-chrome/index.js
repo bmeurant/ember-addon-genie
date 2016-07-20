d0331c40fb0a63b75c698e4525b1bcb0ae3adca0
Baptiste Meurant <baptiste@meurant.io>
2016-07-20 17:16:34 +0200

    Make genie-chrome amending testem instead of replacing it
    Previous implementation was based on a full replacement on testem.js file in order to set configuration for Chrome in CI. This feature is amending the file instead, allowing to keep users modification while setting proper Chrome config. This will allow to have both `genie-chrome` and `genie-coverage` amending the file, for example. Distributing testem.js file in blueprint is not required anymore.

--- blueprints/genie-chrome/index.js
+++ blueprints/genie-chrome/index.js
@@ -9,6 +9,43 @@ module.exports = {
 
   afterInstall: function() {
     this._setupChromeWithTravis();
+    this._modifyTestemJs();
+  },
+
+  _modifyTestemJs: function() {
+    // load testem content as js object for easier manipulations and support of any file structure
+    var testemJs;
+    try {
+      testemJs = require('../../../../testem.js');
+    } catch(err) {
+      console.warn("WARN genie-chrome: testem.js need to be updated but could not be found.");
+      return;
+    }
+
+    // rebuild "launch_in_ci" array
+    testemJs.launch_in_ci = testemJs.launch_in_ci || [];
+    var launchInCi = testemJs.launch_in_ci;
+
+    var indexOfPhantom = launchInCi.indexOf("PhantomJS");
+    if (indexOfPhantom !== -1) {
+      launchInCi.splice(indexOfPhantom);
+    }
+
+    if (launchInCi.indexOf("Chrome") === -1) {
+      launchInCi.push("Chrome");
+    }
+
+    // replace module content
+    var data = utils.getContents.call(this, 'testem.js');
+    var moduleExp = /^((.|\n)*module\.exports\s*=\s*)({(.|\n)*})(;(.|\n)*)/;
+    var regexParts = data.match(moduleExp);
+
+    if (regexParts && regexParts.length >= 5) {
+      data = data.replace(moduleExp, "$1" + JSON.stringify(testemJs, null, 2) + "$5");
+      utils.setContents.call(this, 'testem.js', 'js', data);
+    } else {
+      console.warn("WARN genie-chrome: testem.js could not be modified due to incorrect format. Maybe a lack of a final `;` ?");
+    }
   },
 
   _setupChromeWithTravis: function() {
